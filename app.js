55fe713cd1307c9a9a3e7adf00b723d85087a82e
Aditya Gupta <slimshady9852@gmail.com>
2020-04-14 15:17:49 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const moonPath = "M14.5 27.5C14.5 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 14.5 12.3122 14.5 27.5Z";
+
+const sunPath = "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
+
+const darkMood = document.querySelector('.sun');
+let toggle = false;
+
+darkMood.addEventListener('click', ()=>{
+
+    const timeline = anime.timeline({
+        duration: 750,
+        easing: "easeOutExpo"
+    });
+    timeline.add({
+        targets:'.sun',
+        d: [{value: toggle?sunPath:moonPath}]
+    })
+    .add({
+        targets: '#darkMode',
+        rotate: toggle?-320:320
+    },'-=350')
+    .add({
+        targets: 'section',
+        backgroundColor: toggle?'rgb(199,199,199)':'rgb(22,22,22)',
+        color: toggle?'rgb(22,22,22)':'rgb(255,255,255)'
+    },'-=750')
+
+    if(!toggle){
+        toggle= true;
+    }else{
+        toggle=false;
+    }
+})
